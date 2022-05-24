a0277391212ea46f20ab235de861deacd8f15023
Robinson Idagiza <robinadoro@gmail.com>
2022-05-25 00:06:06 +0300

    ft:Add benz pic on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,14 +4,14 @@ import Benz from "../../assets/benz.png"
 function Home() {
   return (
     <div className='home bg-neutral-400'>
-        <div className='py-9 text-right'>
+        <div className='py-9 text-right '>
             <h3 className='text-3xl'>Know your Dream Car</h3>
             <h5 className='text-xl'>Get the manufacturer information
                  for  your dream car.
             </h5>
         </div>
         <div>
-            <img src={Benz} alt='benz'/>
+            <img src={Benz} alt='benz' className='ml-44 p-8'/>
         </div>
         
     </div>
